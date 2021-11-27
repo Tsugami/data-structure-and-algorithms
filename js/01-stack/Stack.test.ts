d4501272f2b379c8037c455392ad8b69a1f11ce1
Yslan Ramos <yslanramos.r@gmail.com>
2021-11-27 10:39:03 -0300

    refactor: remove repeated name in tests

--- js/01-stack/Stack.test.ts
+++ js/01-stack/Stack.test.ts
@@ -1,123 +1,125 @@
 import Stacks from './solutions';
 
 describe.each(Stacks)('Stack', ({ Stack, name }) => {
-  let stack;
+  describe(name, () => {
+    let stack;
 
-  beforeEach(() => {
-    stack = new Stack<number>();
-  });
+    beforeEach(() => {
+      stack = new Stack<number>();
+    });
 
-  it(`[${name}] starts empty`, () => {
-    expect(stack.size()).toBe(0);
-    expect(stack.isEmpty()).toBe(true);
-  });
+    it('starts empty', () => {
+      expect(stack.size()).toBe(0);
+      expect(stack.isEmpty()).toBe(true);
+    });
 
-  it(`[${name}] pushes elements`, () => {
-    stack.push(1);
-    expect(stack.size()).toBe(1);
-    stack.push(2);
-    expect(stack.size()).toBe(2);
-    stack.push(3);
-    expect(stack.size()).toBe(3);
+    it('pushes elements', () => {
+      stack.push(1);
+      expect(stack.size()).toBe(1);
+      stack.push(2);
+      expect(stack.size()).toBe(2);
+      stack.push(3);
+      expect(stack.size()).toBe(3);
 
-    expect(stack.isEmpty()).toBe(false);
-  });
+      expect(stack.isEmpty()).toBe(false);
+    });
 
-  it(`[${name}] pops elements`, () => {
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+    it('pops elements', () => {
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
 
-    expect(stack.pop()).toBe(3);
-    expect(stack.pop()).toBe(2);
-    expect(stack.pop()).toBe(1);
-    expect(stack.pop()).toBe(undefined);
-  });
+      expect(stack.pop()).toBe(3);
+      expect(stack.pop()).toBe(2);
+      expect(stack.pop()).toBe(1);
+      expect(stack.pop()).toBe(undefined);
+    });
 
-  it(`[${name}] implements LIFO logic`, () => {
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+    it('implements LIFO logic', () => {
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
 
-    expect(stack.pop()).toBe(3);
-    expect(stack.pop()).toBe(2);
-    expect(stack.pop()).toBe(1);
-    expect(stack.pop()).toBe(undefined);
-  });
+      expect(stack.pop()).toBe(3);
+      expect(stack.pop()).toBe(2);
+      expect(stack.pop()).toBe(1);
+      expect(stack.pop()).toBe(undefined);
+    });
 
-  it(`[${name}] allows to peek at the top element in the stack without popping it`, () => {
-    expect(stack.peek()).toBe(undefined);
+    it('allows to peek at the top element in the stack without popping it', () => {
+      expect(stack.peek()).toBe(undefined);
 
-    stack.push(1);
-    expect(stack.peek()).toBe(1);
+      stack.push(1);
+      expect(stack.peek()).toBe(1);
 
-    stack.push(2);
-    expect(stack.peek()).toBe(2);
+      stack.push(2);
+      expect(stack.peek()).toBe(2);
 
-    stack.pop();
-    expect(stack.peek()).toBe(1);
-  });
+      stack.pop();
+      expect(stack.peek()).toBe(1);
+    });
 
-  it(`[${name}] returns the correct size`, () => {
-    expect(stack.size()).toBe(0);
-    stack.push(1);
-    expect(stack.size()).toBe(1);
-    stack.push(2);
-    expect(stack.size()).toBe(2);
-    stack.push(3);
-    expect(stack.size()).toBe(3);
+    it('returns the correct size', () => {
+      expect(stack.size()).toBe(0);
+      stack.push(1);
+      expect(stack.size()).toBe(1);
+      stack.push(2);
+      expect(stack.size()).toBe(2);
+      stack.push(3);
+      expect(stack.size()).toBe(3);
 
-    stack.clear();
-    expect(stack.isEmpty()).toBe(true);
+      stack.clear();
+      expect(stack.isEmpty()).toBe(true);
 
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
 
-    stack.pop();
-    expect(stack.size()).toBe(2);
-    stack.pop();
-    expect(stack.size()).toBe(1);
-    stack.pop();
-    expect(stack.size()).toBe(0);
-    stack.pop();
-    expect(stack.size()).toBe(0);
-  });
+      stack.pop();
+      expect(stack.size()).toBe(2);
+      stack.pop();
+      expect(stack.size()).toBe(1);
+      stack.pop();
+      expect(stack.size()).toBe(0);
+      stack.pop();
+      expect(stack.size()).toBe(0);
+    });
 
-  it(`[${name}] returns if it is empty`, () => {
-    expect(stack.isEmpty()).toBe(true);
-    stack.push(1);
-    expect(stack.isEmpty()).toBe(false);
-    stack.push(2);
-    expect(stack.isEmpty()).toBe(false);
-    stack.push(3);
-    expect(stack.isEmpty()).toBe(false);
+    it('returns if it is empty', () => {
+      expect(stack.isEmpty()).toBe(true);
+      stack.push(1);
+      expect(stack.isEmpty()).toBe(false);
+      stack.push(2);
+      expect(stack.isEmpty()).toBe(false);
+      stack.push(3);
+      expect(stack.isEmpty()).toBe(false);
 
-    stack.clear();
-    expect(stack.isEmpty()).toBe(true);
+      stack.clear();
+      expect(stack.isEmpty()).toBe(true);
 
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
 
-    stack.pop();
-    expect(stack.isEmpty()).toBe(false);
-    stack.pop();
-    expect(stack.isEmpty()).toBe(false);
-    stack.pop();
-    expect(stack.isEmpty()).toBe(true);
-    stack.pop();
-    expect(stack.isEmpty()).toBe(true);
-  });
+      stack.pop();
+      expect(stack.isEmpty()).toBe(false);
+      stack.pop();
+      expect(stack.isEmpty()).toBe(false);
+      stack.pop();
+      expect(stack.isEmpty()).toBe(true);
+      stack.pop();
+      expect(stack.isEmpty()).toBe(true);
+    });
 
-  it(`[${name}] clears the stack`, () => {
-    stack.clear();
-    expect(stack.isEmpty()).toBe(true);
+    it('clears the stack', () => {
+      stack.clear();
+      expect(stack.isEmpty()).toBe(true);
 
-    stack.push(1);
-    stack.push(2);
+      stack.push(1);
+      stack.push(2);
 
-    stack.clear();
-    expect(stack.isEmpty()).toBe(true);
+      stack.clear();
+      expect(stack.isEmpty()).toBe(true);
+    });
   });
 });
--- js/01-stack/challenges/baseConverter.test.ts
+++ js/01-stack/challenges/baseConverter.test.ts
@@ -1,26 +1,28 @@
 import Stacks from '../solutions';
 import { baseConverter } from './baseConverter';
 
-describe.each(Stacks)('Stack', ({ Stack, name }) => {
-  let stack;
+describe.each(Stacks)('baseConverter', ({ Stack, name }) => {
+  describe(name, () => {
+    let stack;
 
-  beforeEach(() => {
-    stack = new Stack<string>();
-  });
+    beforeEach(() => {
+      stack = new Stack<string>();
+    });
 
-  it(`[${name}] should return base 2`, () => {
-    expect(baseConverter(100345, 2, stack)).toBe('11000011111111001');
-  });
+    it('should return base 2', () => {
+      expect(baseConverter(100345, 2, stack)).toBe('11000011111111001');
+    });
 
-  it(`[${name}] should return base 8`, () => {
-    expect(baseConverter(100345, 8, stack)).toBe('303771');
-  });
+    it('should return base 8', () => {
+      expect(baseConverter(100345, 8, stack)).toBe('303771');
+    });
 
-  it(`[${name}] should return base 16`, () => {
-    expect(baseConverter(100345, 16, stack)).toBe('187F9');
-  });
+    it('should return base 16', () => {
+      expect(baseConverter(100345, 16, stack)).toBe('187F9');
+    });
 
-  it(`[${name}] should return base 35`, () => {
-    expect(baseConverter(100345, 35, stack)).toBe('2BW0');
+    it('should return base 35', () => {
+      expect(baseConverter(100345, 35, stack)).toBe('2BW0');
+    });
   });
 });
